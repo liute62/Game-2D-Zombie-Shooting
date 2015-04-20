8238f8cc9e3a23f48641aac48e05e770a4410f75
Max_Liu <liute62@sina.cn>
2015-04-20 11:13:03 -0400

    Add Simple Camera Follow

---
+++ Assets/Scrips/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject Target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Target.transform.localPosition.x;
+		float y = Target.transform.localPosition.y;
+		float z = this.transform.localPosition.z;
+		this.transform.localPosition = new Vector3 (x,y,z);
+	}
+}
