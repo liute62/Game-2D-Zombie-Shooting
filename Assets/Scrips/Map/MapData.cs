a9f700f8a303b48ba704239b5ba1e28069da8735
Max_Liu <liute62@sina.cn>
2015-04-27 21:33:44 -0400

    Add simple game features and game structure plan.

---
+++ Assets/Scrips/Map/MapData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
