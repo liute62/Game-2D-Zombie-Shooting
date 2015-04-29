51d828961541b1c2c3db46c822ec9e96ea102b51
brinegjr <brinegjr@rose-hulman.edu>
2015-04-28 22:36:57 -0400

    Built demo version of first two levels and change
    have working demo of changing levels working, however the movement of
    the camera and zombies are not working for some reason

---
+++ Assets/Scrips/Fading.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading : MonoBehaviour {
+
+	public Texture2D fadeTexture;
+	public float fadeSpeed = 0.8f;
+
+	private int drawDepth = -1000;
+	private float alpha = 1.0f;
+	private int fadeDirection = -1;
+
+	void OnGUI (){
+		alpha += fadeSpeed * fadeDirection * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeTexture);
+
+	}
+
+	public float beginFade (int direction){
+		fadeDirection = direction;
+		return (fadeSpeed);
+	}
+
+	void OnLevelWasLoaded (){
+		beginFade (-1);
+	}
+}
