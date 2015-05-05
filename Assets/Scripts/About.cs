c562cf689ccf5e5f9f6361c73e79a5129e865b7e
XTLi <xtli1689@163.com>
2015-05-04 21:35:41 -0400

    GUI_part

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class About : MonoBehaviour {
+
+	public TweenPosition mainMenu;
+	public TweenPosition aboutMenu;
+	
+	// Use this for initialization
+	//	void Start () {
+	//	
+	//	}
+	
+	// Update is called once per frame
+	//	void Update () {
+	//	
+	//	}
+	
+	public void ClickAboutButton(){
+		mainMenu.PlayForward ();
+		aboutMenu.PlayForward ();
+	}
+	
+	public void ClickBackButton(){
+		mainMenu.PlayReverse ();
+		aboutMenu.PlayReverse ();
+	}
+}
---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+	public TweenPosition mainMenu;
+	public TweenPosition controlMenu;
+	
+	// Use this for initialization
+	//	void Start () {
+	//	
+	//	}
+	
+	// Update is called once per frame
+	//	void Update () {
+	//	
+	//	}
+	
+	public void ClickControlButton(){
+		mainMenu.PlayForward ();
+		controlMenu.PlayForward ();
+	}
+	
+	public void ClickBackButton(){
+		mainMenu.PlayReverse ();
+		controlMenu.PlayReverse ();
+	}
+}
