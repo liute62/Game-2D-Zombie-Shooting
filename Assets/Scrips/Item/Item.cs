02ad50657bba10f5af89c4423287165d84797d5a
Max_Liu <liute62@sina.cn>
2015-05-10 13:53:39 -0400

    Add Key prefab

---
+++ Assets/Scrips/Item/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Ontrigger");
+		if (other.gameObject.Equals (player)) {
+			//if it is the player get the key
+			Destroy(this.gameObject);
+		}
+	 }
+
+  }
